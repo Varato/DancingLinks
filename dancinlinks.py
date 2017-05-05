91e995d26e1304abd6fc5e9238c8f5f9c7eec1c1
Varato <imxinchen@outlook.com>
2017-05-05 12:54:50 +0800

    xiaoling

--- dancinlinks.py
+++ dancinlinks.py
@@ -44,49 +44,61 @@ class Node:
     def C(self):
         return self.c
 
-def mat2linklist(A):
+def mat2linklist(A, column_names=None):
     """
     A: 0-1 array
     """
     root = Root()
     tmp1 = root
-    nodes = []
     r_n,c_n = A.shape 
+    nodes = [None] * c_n
     for j in range(c_n):
         column_header = Column()
+        if column_names:
+            column_header.n = column_names[j]
+        else:
+            column_header.n = j
         tmp2 = column_header
-        
         tmp1.r = column_header
         column_header.l = tmp1
         tmp1 = column_header
-        column = []
+        column = [None] * r_n
         for i in range(r_n):
             if A[i,j] == 1:
+                column_header.s += 1
                 node = Node()
-                column.append(node)
-                node.c = column
+                column[i] = node
+                node.c = column_header
                 tmp2.d = node
                 node.u = tmp2
                 tmp2 = node
-            else:
-                column.append(None)
-        nodes.append(column)
-                
+        nodes[j] = column             
         tmp2.d = column_header
         column_header.u = tmp2
+
     tmp1.r=root
-    root.l=tmp1    
+    root.l=tmp1
     
     for i in range(r_n):
         for j in range(c_n):
             if A[i,j] == 1:
                 tmp = nodes[j][i]
-                # searches the next 1 on the right
+                # keeps searching the next 1 on the right
                 for k in range(1, c_n+1):
                     if A[i, np.mod(j+k, c_n)]==1:
-                        node_r = nodes[j+k][i]
+                        node_r = nodes[np.mod(j+k,c_n)][i]
                         tmp.r = node_r
                         node_r.l = tmp
                         tmp = node_r
                 break
     return root
+
+if __name__ == "__main__":
+    A = np.array([[0,0,1,0,1,1,0],
+                  [1,0,0,1,0,0,1],
+                  [0,1,1,0,0,1,0],
+                  [1,0,0,1,0,0,0],
+                  [0,1,0,0,0,0,1],
+                  [0,0,0,1,1,0,1]])
+
+    root0 = mat2linklist(A)
